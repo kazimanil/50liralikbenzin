3626e8770f88bcc3d9170c7d8baa7b4dd88f5510
Kazım Anıl Eren <kazim.eren@deliveryhero.com>
2024-01-05 13:07:35 +0100

    Kodlar güncellendi

--- 50liralikbenzin.R
+++ 50liralikbenzin.R
@@ -1,7 +1,12 @@
-# Data Input -----
+# Setup ----
 rm(list = ls()); gc()
-source("https://raw.githubusercontent.com/kazimanil/project_kaf/master/gg_theme.R") # My Theme for GGPlot2
+source("https://raw.githubusercontent.com/kazimanil/project_kaf/master/R_functions/gg_theme.R") # My Theme for GGPlot2
+library("data.table");
+library("ggplot2");
+setwd("C:/Users/k.eren/Documents/50liralikbenzin/")
+# Data Input -----
 veri <- fread("HamVeri.csv")
+enflasyon <- fread("tuik_enflasyon.csv")
 veri[, ':='( 
 	tarih = as.Date(tarih, format = "%d/%m/%Y"),
 	kursunsuz = as.numeric(gsub(pattern = ",", replacement = ".", x = kursunsuz)),
@@ -17,7 +22,6 @@ veri[tarih < as.Date("2008-01-01"), BenzinTuk := 6.3]
 veri[tarih >= as.Date("2008-01-01") & tarih < as.Date("2012-01-01"), BenzinTuk := 6.2]
 veri[tarih >= as.Date("2012-01-01") & tarih < as.Date("2019-01-01"), BenzinTuk := 5.2]
 veri[tarih >= as.Date("2019-01-01"), BenzinTuk := 4.8]
-
 veri[, ':='(kursunsuz50gy = 100 * kursunsuz50 / BenzinTuk)]
 
 # Now let's check how many increments have happened over years.
@@ -46,11 +50,47 @@ veri_yil = merge(
 	); rm(veri_yil2)
 veri_yil[, toplam_TL := ort_TL * adet]
 
+# Starting from 2020, the absolute change in the TL / LT price has started making huge spikes
+# compared to the past. Hence, I need to calculate the percentage change for a better visualisation.
+
+yillik = veri[, .(yilbasi = min(tarih), yilsonu = max(tarih)), .(yil = year(tarih))]
+yillik = merge(
+  yillik,
+  veri[, .(tarih, yilbasi_fiyat = kursunsuz)],
+  by.x = "yilbasi",
+  by.y = "tarih"
+);
+
+yillik = merge(
+  yillik,
+  veri[, .(tarih, yilsonu_fiyat = kursunsuz)],
+  by.x = "yilsonu",
+  by.y = "tarih"
+);
+
+yillik = yillik[, .(
+  yil,
+  yilbasi_fiyat,
+  mutlak_degisim = (yilsonu_fiyat - yilbasi_fiyat), 
+  yuzde_degisim = 100 * (yilsonu_fiyat - yilbasi_fiyat) / yilbasi_fiyat
+  )]
+
+# now let's get the TURKSTAT's yearly inflation rates for a comparison.
+enflasyon = enflasyon[month(yilay) == 1]
+enflasyon[, onceki_bazpuan := shift(bazpuan, 1)]
+enflasyon = enflasyon[!is.na(onceki_bazpuan), .(yil = year(yilay), enflasyon = (bazpuan - onceki_bazpuan) / onceki_bazpuan)]
+
+enflasyon = merge(
+  yillik,
+  enflasyon,
+  by = "yil"
+)
+
 # Graphics Output ----
 dev.off()
 jpeg(filename = "Grafik_1.jpeg", width = 1024, height = 768)
 ggplot(data = veri, aes(x = tarih, y = kursunsuz50)) +
-	geom_line(size = 1, linetype = "dotted", colour = "orange") +
+	geom_line(linewidth = 1, linetype = "dotted", colour = "orange") +
 	geom_smooth(size = 2, color = "turquoise", se = FALSE,  method = "lm", formula = y ~ poly(x,4)) + # geom_smooth(color = "red") +
 	labs(title = "50 Lira ile Kaç Litre Benzin Alınabiliyor?", x = "Tarih", y = "Kurşunsuz Benzin (Lt.)") +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
@@ -61,45 +101,105 @@ dev.off()
 
 jpeg(filename = "Grafik_2.jpeg", width = 1024, height = 768)
 ggplot(data = veri, aes(x = tarih, y = kursunsuz50gy)) +
-	geom_line(size = 1, linetype = "dotted", colour = "orange") +
+	geom_line(linewidth = 1, linetype = "dotted", colour = "orange") +
 	geom_smooth(size = 2, color = "turquoise", se = FALSE, method = "lm", formula = y ~ poly(x,4)) + # geom_smooth(color = "red") +
 	labs(title = "50 liralık benzin ile kaç km. yol gidiliyor?", x = "Tarih", y = "Ortalama Gidilen Yol (Km.)") +
   scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
-  scale_y_continuous(breaks = seq(100, 300, 25)) +
+  scale_y_continuous(breaks = seq(25, 300, 25)) +
 	gg_theme() +
 	theme(text = element_text(size=30))
 dev.off()
 
 jpeg(filename = "Grafik_3.jpeg", 1024, 768)
-ggplot(data = veri_yil[yil != 2020], aes(x = yil, y = adet, fill = degisim)) +
+ggplot(data = veri_yil, aes(x = yil, y = adet, fill = degisim)) +
 	geom_col(position = "dodge") +
 	labs(title = "Senelik Zam ve İndirim Adetleri", x = "Yıl", y = "Adet") +
 	scale_fill_discrete(
-		name = "Güncelleme Tipi\n",
-		breaks = c("Zam", "indirim"),
+	  name = "Değişim Tipi \n",
+	  breaks = c("Zam", "indirim"),
 		labels = c(" Zam", " İndirim")
 		) +
-  scale_x_continuous(breaks = seq(2007, 2019, 1)) +
-  scale_y_continuous(breaks = seq(0, 30, 5)) +
+  scale_x_continuous(breaks = seq(2007, 2023, 1)) +
+  scale_y_continuous(breaks = seq(0, 60, 10)) +
 	gg_theme() +
-	theme(text = element_text(size=30))
+	theme(text = element_text(size=30), legend.position = "bottom")
 dev.off()
 
 jpeg(filename = "Grafik_4.jpeg", 1024, 768)
-ggplot(data = veri_yil[yil != 2020], aes(x = yil, y = toplam_TL, fill = degisim)) +
+ggplot(data = veri_yil[yil < 2021], aes(x = yil, y = toplam_TL, fill = degisim)) +
 	geom_col(position = "dodge") +
 	labs(title = "Senelik Zam ve İndirim Miktarları (TL)", x = "Yıl", y = "TL") +
   scale_fill_discrete(
-	name = "Güncelleme Tipi\n",
+  name = "Değişim Tipi \n",
 	breaks = c("Zam", "indirim"),
 	labels = c("Toplam Yapılan\nSenelik Zam (TL)", "Toplam Yapılan\nSenelik İndirim (TL)")
 	) +
-  scale_x_continuous(breaks = seq(2007, 2019, 1)) +
-  scale_y_continuous(breaks = seq(0, 3, .25)) +
+  scale_x_continuous(breaks = seq(2007, 2020, 1)) +
+  scale_y_continuous(breaks = seq(0, 5, 0.5)) +
 	gg_theme() +
-	theme(text = element_text(size=30))
+	theme(text = element_text(size=30), legend.position = "bottom")
 dev.off()
 
+jpeg(filename = "Grafik_4b.jpeg", 1024, 768)
+ggplot(data = veri_yil[yil > 2019], aes(x = yil, y = toplam_TL, fill = degisim)) +
+  geom_col(position = "dodge") +
+  labs(title = "Senelik Zam ve İndirim Miktarları (TL)", x = "Yıl", y = "TL") +
+  scale_fill_discrete(
+    name = "Değişim Tipi \n",
+    breaks = c("Zam", "indirim"),
+    labels = c("Toplam Yapılan\nSenelik Zam (TL)", "Toplam Yapılan\nSenelik İndirim (TL)")
+  ) +
+  scale_x_continuous(breaks = seq(2020, 2023, 1)) +
+  scale_y_continuous(breaks = seq(0, 30, 5)) +
+  gg_theme() +
+  theme(text = element_text(size=30), legend.position = "bottom")
+dev.off()
+
+jpeg(filename = "Grafik_5.jpeg", 1024, 768)
+ggplot(data = enflasyon, aes(x = yil)) +
+  geom_bar(
+    stat= "identity",
+    aes(y = mutlak_degisim),
+    fill = "turquoise"
+    ) +
+  geom_line(
+    aes(y = yuzde_degisim / 5), 
+    size = 2,
+    col = "red"
+    ) +
+  labs(title = "Senelik Değişim", x = "Yıl", y = "TL (Mutlak)") +
+  scale_x_continuous(breaks = seq(2007, 2023, 1)) +
+  scale_y_continuous(
+    sec.axis = sec_axis(
+      trans = ~.*5,
+      name = "% Değişim")) +
+  gg_theme() +
+  theme(text = element_text(size=30))
+dev.off()
+
+korelasyon = round(100 * cor(enflasyon$yuzde_degisim, enflasyon$enflasyon))
+jpeg(filename = "Grafik_6.jpeg", 1024, 768)
+ggplot(data = enflasyon) +
+  geom_line(
+    aes(x = yil, y = enflasyon * 100, colour = "TUIK"),
+    linewidth = 2
+  ) +
+  geom_line(
+    aes(x = yil, y = yuzde_degisim, colour = "Benzin"),
+    linewidth = 2
+  ) +
+  labs(
+    title = "İki farklı gösterge ile Enflasyon", 
+    subtitle = paste0("Korelasyon: %", korelasyon),
+    colour = "",
+    x = "Yıl", 
+    y = "%"
+    ) +
+  scale_x_continuous(breaks = seq(2007, 2023, 1)) +
+  scale_y_continuous(breaks = seq(-10, 80, 10)) +
+  gg_theme() +
+  theme(text = element_text(size=30), legend.position = "bottom")
+dev.off()
 
 # Daha önceden açıklama kısmında yer alan bu güncelleme notlarını kodlara taşımayı uygun gördüm.
 # # Güncellemeler
